3c832405f2fb2acea55879e3d20125bdf17cf3a7
Son Nguyen <son.nguyen@enablestartup.com>
2021-05-11 16:33:25 +0700

    feature/api-for-team

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ $router->get('/', function () use ($router) {
     return $router->app->version();
 });
 
-
+// Api for User
 $router->group(['prefix' => 'api'], function () use ($router) {
     $router->get('user',  ['uses' => 'UserController@index']);
 
@@ -28,4 +28,18 @@ $router->group(['prefix' => 'api'], function () use ($router) {
     $router->delete('user/{id}', ['uses' => 'UserController@delete']);
 
     $router->put('user/{id}', ['uses' => 'UserController@update']);
+});
+
+
+// Api for Team
+$router->group(['prefix' => 'api'], function () use ($router) {
+    $router->get('team',  ['uses' => 'TeamController@index']);
+
+    $router->get('team/{id}',  ['uses' => 'TeamController@show']);
+
+    $router->post('team',  ['uses' => 'TeamController@store']);
+
+    $router->put('team/{id}', ['uses' => 'TeamController@update']);
+
+    $router->delete('team/{id}', ['uses' => 'TeamController@delete']);
 });
