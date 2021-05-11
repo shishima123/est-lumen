674ece8c56c701eff7d8d2913cbe109b23bc2461
vohongnga <ngachedunhubo99>
2021-05-11 11:58:11 +0700

    fix redundant file

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers;
 
 use Laravel\Lumen\Routing\Controller as BaseController;
-
+use Illuminate\Support\Facades\Auth;
 class Controller extends BaseController
 {
-    //
+    
 }
