82af355f550ba0fb4ecf45acbcc83a52b93dd675
Son Nguyen <son.nguyen@enablestartup.com>
2021-05-13 15:35:26 +0700

    fix-bug

--- app/Http/Controllers/UserTeamController.php
+++ app/Http/Controllers/UserTeamController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Repositories\UserTeamRepository;
-use Illuminate\Support\Facades\DB;
 use Validator;
 
 class UserTeamController extends Controller
