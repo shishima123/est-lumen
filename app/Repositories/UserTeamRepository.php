7673abbcf8b41979697502894b59bf7727c5fd5c
Son Nguyen <son.nguyen@enablestartup.com>
2021-05-26 11:47:14 +0700

    feature/add-parameters-paginate

--- app/Repositories/UserTeamRepository.php
+++ app/Repositories/UserTeamRepository.php
@@ -6,6 +6,7 @@ use App\Repositories\RepositoryAbstract;
 use App\Models\UserTeam;
 use App\Enum\Paginate;
 use App\Enum\RoleUserTeam;
+
 /**
  * Class UserTeamRepository
  *
@@ -24,27 +25,31 @@ class UserTeamRepository extends RepositoryAbstract
     }
 
     // Get all data
-    public function getData()
+    public function getData($perPage)
     {
+        if (is_numeric($perPage)) {
+            return $this->model->paginate($perPage);
+        }
+
         return  $this->model->paginate(Paginate::PAGINATE);
     }
 
-    public function findUserInTeam($user_id,$team_id)
+    public function findUserInTeam($user_id, $team_id)
     {
-        return $this->model->where('user_id',$user_id)->where('team_id',$team_id)->first();
+        return $this->model->where('user_id', $user_id)->where('team_id', $team_id)->first();
     }
-    public function changeAdmin($id,$role_id)
+    public function changeAdmin($id, $role_id)
     {
-        return $this->model->where('id',$id)->update(['role'=>$role_id]);
+        return $this->model->where('id', $id)->update(['role' => $role_id]);
     }
 
-    public function checkPermission ($user_id,$team_id)
+    public function checkPermission($user_id, $team_id)
     {
-        $userInTeam =  $this->findUserInTeam($user_id,$team_id);
+        $userInTeam =  $this->findUserInTeam($user_id, $team_id);
         $msg = '';
-        if($userInTeam != null){
-            if($userInTeam->isMemberRole()){
-                $msg = "You don't have permission for that" ;
+        if ($userInTeam != null) {
+            if ($userInTeam->isMemberRole()) {
+                $msg = "You don't have permission for that";
             }
         }
         return $msg;
@@ -52,6 +57,6 @@ class UserTeamRepository extends RepositoryAbstract
 
     public function deleteMultiple($arrayId)
     {
-        return $this->model->whereIn('user_id',$arrayId)->delete();
+        return $this->model->whereIn('user_id', $arrayId)->delete();
     }
 }
