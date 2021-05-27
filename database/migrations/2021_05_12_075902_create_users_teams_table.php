f4aef031b0f7e2bcb631f164d25613c310e8baea
Son Nguyen <son.nguyen@enablestartup.com>
2021-05-27 16:08:56 +0700

    fix-bug/seed-db

--- database/migrations/2021_05_12_075902_create_users_teams_table.php
+++ database/migrations/2021_05_12_075902_create_users_teams_table.php
@@ -3,7 +3,6 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-use Illuminate\Support\Facades\DB;
 
 class CreateUsersTeamsTable extends Migration
 {
@@ -36,8 +35,6 @@ class CreateUsersTeamsTable extends Migration
      */
     public function down()
     {
-        DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         Schema::dropIfExists('users_teams');
-        DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
 }
